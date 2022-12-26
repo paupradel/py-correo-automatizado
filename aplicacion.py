c9967f87b7df468e3a3f9b8cb1a9ee67031bf5e0
Paulina Pradel <pradel.paulina@gmail.com>
2022-12-25 19:12:45 -0600

    primer commit

---
+++ aplicacion.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+# Autenticación en google drive
+
+
+# Obtención del archivo de google drive
+
+
+# Asignación de variables para construir el correo
+
+
+# Construcción del correo
+
+
+# Envio de correos
+
